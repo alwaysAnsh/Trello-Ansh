227591dab7c0fa763d6af7b66de9a2b57446c309
alwaysAnsh <anshj9y@gamil.com>
2024-07-29 22:55:32 +0530

    ignore

--- server/index.js
+++ server/index.js
@@ -29,10 +29,14 @@ app.use(cors({
 app.use('/api/v1', userRoutes);
 app.use('/api/v1', taskRoutes);
 
-app.use(express.static(path.join(__dirname, '/client/dist')))
+// app.use(express.static(path.join(__dirname, '/client/dist')))
+app.use(express.static(path.join(__dirname, '..', 'client', 'dist')));
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'client','dist','index.html'))
+// })
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client','dist','index.html'))
-})
+    res.sendFile(path.join(__dirname, '..', 'client', 'dist', 'index.html'));
+});
 
 app.get('/', (req,res) => res.send("this is homepage for backend server. Hellooooo"))
 
